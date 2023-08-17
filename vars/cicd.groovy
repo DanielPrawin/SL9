7938b9a7993e48093d97688ea78b3cb1b1cdcdc3
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-17 20:57:15 +0530

    Update cicd.groovy2

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,16 +1,16 @@
 def newGit(repo)
 {
-  git 'https://github.com/DanielPrawin/${repo}.git'
+  git "https://github.com/DanielPrawin/${repo}.git"
 }
 def build()
 {
-   sh 'mvn package'
+   sh "mvn package"
 }
 def copyBuilt(jobName,Ip,contextName)
 {
-  sh 'scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${Ip}:/var/lib/tomcat9/webapps/${contextName}.war'
+  sh "scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${Ip}:/var/lib/tomcat9/webapps/${contextName}.war"
 }
 def test(jobName)
 {
-  sh 'java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar'
+  sh "java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar"
 }
