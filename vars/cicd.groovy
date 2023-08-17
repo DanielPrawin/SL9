792ff3c642de7b9527c4138382d2108e146e8e24
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-08-17 20:29:31 +0530

    Create cicd.groovy1

---
+++ vars/cicd.groovy
@@ -0,0 +1,16 @@
+def newGit(repo)
+{
+  git 'https://github.com/DanielPrawin/${repo}.git'
+}
+def build()
+{
+   sh 'mvn package'
+}
+def copyBuilt(jobName,Ip,contextName)
+{
+  sh 'scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${Ip}:/var/lib/tomcat9/webapps/${contextName}.war'
+}
+def test(jobName)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${jobName}/testing.jar'
+}
